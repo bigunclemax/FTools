95e52013496e681bdb493ef4ab633e99751d0e7d
kiselev.m <bigunclemax@gmail.com>
2020-04-19 20:10:58 +0300

    pack.sh [ref] fix warnings [add] use repacked image section (packed.bin) instead original

--- tools/pack.sh
+++ tools/pack.sh
@@ -5,15 +5,17 @@ set -euo pipefail
 export PATH=$PATH:../VbfEditor/bin:../EifViewer/bin:../ImgSectionParser/bin
 
 function pack_images() {
-  local cfg_file=$1/_config.json
-  local img_dir=$(dirname $1)/images
-  imgparcer -p ${cfg_file} -o $1/../_packed.bin
+  local cfg_file=$(realpath "$1"/*_config.json)
+  local img_dir=$(dirname "$1")/images
+  imgparcer -p "$cfg_file" -o "$1"/../_packed.bin
 }
 
 function pack_vbf() {
-  local vbf=$1/*_config.json
-  pack_images $(echo ${1}/images/)
-  vbfeditor -p ${vbf} -o ${1}/../packed.vbf
+  local vbf_conifg=$(realpath "$1"/*_config.json)
+  pack_images "$1"/images/
+  #  local img_sec_name=$(sed  -n '9s/.*file.*"\(.*\)".*/_packed.bin/p' "${vbf_conifg}")
+  sed -i "9s/:.*\"/: \"_packed.bin\"/" "${vbf_conifg}"
+  vbfeditor -p "$vbf_conifg" -o "$1"/../packed.vbf
 }
 
 if [ "$#" -ne 1 ]; then
